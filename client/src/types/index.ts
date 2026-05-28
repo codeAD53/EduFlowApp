975fe4ce511e59673cfba73a62fc25c269ba34bc
codeAD53 <adinayak2026@gmail.com>
2026-05-28 19:35:35 +0530

    Adding Roadpage View

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -56,4 +56,4 @@ export interface RoadmapProgress {
   }[]
 }
 
-export type ProgressStatus = 'not_started' | 'in_progress' | 'completed'
+export type ProgressStatus = 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED'
