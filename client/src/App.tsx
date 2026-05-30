a3f54f3dfa04fdf34afbd2dd6f86724df7984337
codeAD53 <adinayak2026@gmail.com>
2026-05-30 20:54:59 +0530

    Final Polishing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './contexts/AuthContext'
 import ProtectedRoute from './components/ProtectedRoutes'
--- client/src/pages/notFound.tsx
+++ client/src/pages/notFound.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion';
 const NotFound = () => {
@@ -10,7 +10,7 @@ const NotFound = () => {
             initial={{opacity:0, y:20}}
             animate={{opacity:1, y:0}}
             className='text-center'>
-                <p className="text-6xl font-bold text-indigo-500/20 mb-4">404</p>
+                <p className="text-6xl font-bold text-indigo-500 mb-4">404</p>
                 <h1 className="text-2xl font-bold text-white mb-2">Page not Found</h1>
                 <p className="text-gray-500 mb-8">The Page you're looking for doesn't exist</p>
                 <button onClick={()=>navigate('/')} className='bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-3 rounded-lg transition'>Go Home</button>
